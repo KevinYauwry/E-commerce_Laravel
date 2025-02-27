0ebb3a796c9668855f5e6eafc21cbd5fd476fe38
Kevin Yauwry <kevinyauwri01@gmail.com>
2025-02-27 22:45:38 +0700

    Adding Shop Interface
    - Adding new blades (about, ccontacts, blog, and cart)
    - Putting the newsletter section to footer blade
    -  adding css for shop page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,12 +3,12 @@
             
             <div>
                 <ul id="navbar">
-                    <li><a class="active" href="{{ route('home') }}">Home</a></li>
-                    <li><a href="{{ route('shop') }}">Shop</li>
-                    <li><a href="{{ route('blog') }}">Blog</a></li>
-                    <li><a href="{{ route('about') }}">About</a></li>
-                    <li><a href="{{ route('contact') }}">Contact</a></li>
-                    <li><a href="{{ route('cart') }}"><i class="fa-solid fa-bag-shopping"></i></a></li>
+                    <li><a class="{{ Route::currentRouteName() == 'home' ? 'active' : '' }}" href="{{ route('home') }}">Home</a></li>
+                    <li><a class="{{ Route::currentRouteName() == 'shop' ? 'active' : '' }}" href="{{ route('shop') }}">Shop</a></li>
+                    <li><a class="{{ Route::currentRouteName() == 'blog' ? 'active' : '' }}" href="{{ route('blog') }}">Blog</a></li>
+                    <li><a class="{{ Route::currentRouteName() == 'about' ? 'active' : '' }}" href="{{ route('about') }}">About</a></li>
+                    <li><a class="{{ Route::currentRouteName() == 'contact' ? 'active' : '' }}" href="{{ route('contact') }}">Contact</a></li>
+                    <li><a class="{{ Route::currentRouteName() == 'cart' ? 'active' : '' }}" href="{{ route('cart') }}"><i class="fa-solid fa-bag-shopping"></i></a></li>
                 </ul>
             </div>
 </section>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,6 +4,277 @@
 
 @section('content')
 
-    <h2>HALO INI PAGE SHOP</h2>
+<section id="page-header">
+            <h2>#VacationVibes</h2>
+            <p>See our newest Summer Collection</p>
+        </section>
+
+        <section id="product1" class="section-p1">
+            <div class="pro-container">
+                <div class="pro">
+                    <img src="{{ asset('images/products/f1.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Botanical Flowers T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 349.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f2.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Blossom Breeze T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 349.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f3.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Floral Essence T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 349.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f4.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Vivid Vines T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 349.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f5.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Lush Petals T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 349.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f6.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Hibiscus Hues T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 439.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f7.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Daisy Drifters Pants</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 549.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/f8.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Ethereal Bloom T-Shirts</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 349.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+
+                <div class="pro">
+                    <img src="{{ asset('images/products/n1.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Light Blue Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n2.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Gray Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n3.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Original White Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n4.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Vivid Vines T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n5.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Denim T-Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n6.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Light Gray Short</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n7.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Soft Brown Shirt</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 249.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+                <div class="pro">
+                    <img src="{{ asset('images/products/n8.jpg')}}" alt="">
+                    <div class="des">
+                        <span>adidas</span>
+                        <h5>Original Black T-Shirts</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rp. 199.000</h4>
+                    </div>
+                    <a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+                </div>
+            </div>
+        </section>
+
+        <section id="pagination" class="section-p1">
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <a href="#"><i class="fa-solid fa-arrow-right"></i></a>
+        </section>
 
 @endsection
